b7f11bd143452b7ab09a78bf97494e202fe049c3
DerekCresswell <derekjcress@gmail.com>
2019-08-26 19:55:35 -0700

    Expanded RigidBody

--- RigidBodySimulator/RigidBody.cpp
+++ RigidBodySimulator/RigidBody.cpp
@@ -5,6 +5,36 @@ class RigidBody {
 
 		float mass;
 
+		//Based on center of object
+		position worldPosition;
+
+		float xVelocity, yVelocity;
+
 		bool isStatic = false;
+
+		//To be called once per FixedUpdate
+		void ApplyGravity(float gravity){
+			
+			if(!isStatic){
+				
+
+
+			}
+
+		}
+
+		void MoveBody(){
+		
+			worldPosition.x += xVelocity;
+			worldPosition.y += yVelocity;
+
+		}
+
+};
+
+struct position {
+
+	float x;
+	float y;
 
 };
--- RigidBodySimulator/Simulator.cpp
+++ RigidBodySimulator/Simulator.cpp
@@ -6,8 +6,31 @@ using namespace std;
 
 class Simulator {
 
+	//Constants for world
+	float GRAVITY = -9.8;
+
 	vector<RigidBody> bodies;
 
-	
+	public:
+
+		//Used for rendering and non physics
+		void Update(){
+		
+
+
+		}
+
+		//Only for physics based events / interactions
+		void FixedUpdate(){
+		
+			for (RigidBody body : bodies){
+
+				body.ApplyGravity(GRAVITY);
+
+				body.MoveBody();
+
+			}
+
+		}
 
 };
